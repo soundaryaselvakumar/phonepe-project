4bb229955500eb102753f00e6766df6cc1b93b63
soundaryaselvakumar <130196330+soundaryaselvakumar@users.noreply.github.com>
2023-05-10 21:32:10 +0530

    Add files via upload

---
+++ Phonepe Project tables.sql
@@ -0,0 +1,9 @@
+use project_phonepe;
+select * from data_aggregated_transaction_table;
+select * from data_aggregated_user_summary_table;
+select * from data_aggregated_user_table;
+select * from data_map_districts_longitude_latitude;
+select * from data_map_indiastates_tu;
+select * from data_map_transaction_table;
+select * from data_map_user_table;
+select * from longitude_latitude_state_table;
